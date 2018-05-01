36056e82cd586e54df5d9f81e7dbc4fcc795f037
patrickatcw <pj8392@yahoo.com>
2018-05-01 17:07:09 -0400

    step 8 for bonus, back to main

--- src/com/me/X.java
+++ src/com/me/X.java
@@ -1,5 +1,7 @@
 package com.me;
 
+import java.util.Scanner;
+
 //step 1 create class
 public class X {
 
@@ -7,8 +9,17 @@ public class X {
     private int x;
 
     //step 3 constructor
-    public X(int x) {
+    /*public X(int x) {
         this.x = x;
+    }*/
+
+    //step 8 bonus
+    //change the above to....
+    public X(Scanner x) {
+        System.out.println("Please enter a number: ");
+        this.x = x.nextInt();
+
+        //step 9 to main
     }
 
     //step 4 method
