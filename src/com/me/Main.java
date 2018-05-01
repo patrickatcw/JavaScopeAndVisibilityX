9e47418e5835ed0b595a78d6852d93e673b9b015
patrickatcw <pj8392@yahoo.com>
2018-05-01 16:59:30 -0400

    initial structure, updated gitignore

---
+++ src/com/me/Main.java
@@ -0,0 +1,24 @@
+package com.me;
+/*
+directions;
+- write a small program to read an integer from the keyboard using scanner and print out
+the times table for that number, the table should run from 1 to 12
+- you are allowed to use one variable called scanner for your scanner instance
+- you can use as many other variables as you need, but they must all be called x,
+which includes any class instances and loop control variables that you may decide
+to use
+- if you use a class, the class can be called X (capital), but any instances of it must
+be called x (lower case)
+- any methods you create must also be called x
+- bonus;
+change your program so that ALL  variables (including the scanner instance) are
+called x
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
